ffdcb0c82e9365cdfcca0e21a931c1d16df12b3b
neslihanaydin3 <neslihanaydin3@garantibbva.com.tr>
2024-10-03 22:26:30 +0300

    Create personel.js

---
+++ personel.js
@@ -0,0 +1,13 @@
+{
+	"person": {
+		"name": "Neslihan Aydın",
+		"age": 29,
+		"address": {
+			"street": "Maltepe",
+			"city": "Istanbul",
+			"state": "NA",
+			"zipcode": "2345"
+		},
+		"email": "neslihan@gmail.com"
+	}
+}
